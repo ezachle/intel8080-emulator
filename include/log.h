f7b6a34d8f7c5c825e1d1578fc4f7464be7426fe
Zach <zacharyle25@gmail.com>
2026-05-07 07:59:30 -0400

    Added logging macros

---
+++ include/log.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define LOG_WARNING(pc, format, ...) printf("WARNING | PC: 0x%04X | " format "\n", pc, __VA_ARGS__)
+#define LOG_DEBUG(pc, format, ...)   printf("DEBUG | PC: 0x%04X | " format "\n", pc, __VA_ARGS__)
+#define LOG_ERROR(pc, format, ...)   printf("ERROR | PC: 0x%04X | " format "\n", pc, __VA_ARGS__)
