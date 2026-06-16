5d2982e8a2eaf3fd703823d83d5214a56ac6dcb4
zach <zacharyle25@gmail.com>
2026-06-16 18:22:15 -0400

    Committing first implementation of space invaders - basic I/O, created display function that walks through the VRAM to display black and white pixels, and created basic timer loop that'll delay the next ~33k cycles if the program is ahead.

--- space_invaders/space_invaders.c
+++ space_invaders/space_invaders.c
@@ -1,21 +1,199 @@
 #include <raylib.h>
-#include "intel8080.h"
+#include "space_invaders.h"
+#include "opcode.h"
 
-int main(int argc, char *argv) {
-    intel8080 *cpu = NULL;
-    if(!init_8080(cpu, "./invaders.rom", 0x0000)) {
+static void handle_input(SpaceInvaders *machine) {
+    // test setting to 0?
+    //  prediction is there might be a stutter....
+    uint8_t port1 = 0x03;
+    uint8_t port2 = 0;
+
+    uint16_t keycode = GetKeyPressed();
+    switch(keycode) {
+        case KEY_C:
+            port1 |= CREDIT;
+            break;
+        case KEY_ESCAPE:
+            machine->i8080.quit = true;
+            break;
+        case KEY_ONE:
+            port1 |= START_1P;
+            break;
+        case KEY_TWO:
+            port2 |= START_2P;
+            break;
+        case KEY_LEFT:
+            port1 |= LEFT_1P;
+            port2 |= LEFT_2P;
+            break;
+        case KEY_RIGHT:
+            port1 |= RIGHT_1P;
+            port2 |= RIGHT_2P;
+            break;
+        case KEY_SPACE:
+            port1 |= SHOOT_1P;
+            port2 |= SHOOT_2P;
+            break;
+        default:
+            break;
+    };
+
+    machine->i8080.in_reg[1] = port1;
+    machine->i8080.in_reg[2] = port2;
+}
+
+static void draw_display(SpaceInvaders *machine) {
+    /*
+     * Screen was physically rotated counter-CW. (0,0) instead started
+     * on the bottom left.
+     */
+    uint8_t *vram    = &machine->i8080.mem.data[VRAM_START]; // VRAM start 0x2400 - 0x3FFF
+    uint8_t  scale   = machine->io.scale_factor;
+
+    for(uint16_t i = 0; i < VRAM_SIZE; i++) {
+        // rows are in 8-bit chunks
+        int x = (i % 32) * 8;
+        int y = i / 32;
+
+        uint8_t byte = vram[i];
+
+        // iterate through each bit
+        for(uint8_t bit = 0; bit < 8; bit++) {
+            Color pixel = ((byte >> bit) & 1) ? BLACK : WHITE;
+
+            int src_x = y;
+            int src_y = 255 - (x + bit);
+
+            DrawRectangle(
+                src_x * scale,
+                src_y * scale,
+                scale,
+                scale,
+                pixel);
+        }
+    }
+}
+
+void space_invaders_in(intel8080 *i8080, uint8_t port) {
+/*
+ * Read
+ *  00 INPUTS (Mapped in hardware but never used by the code)
+ *  01 INPUTS
+ *  02 INPUTS
+ *  03 bit shift register read
+ *
+ */
+    SpaceInvaders *machine = (SpaceInvaders*)i8080->userdata;
+    switch(port) {
+        case 0x03:
+            // dedicated shift HW to position a 8-bit pixel image into a 16-bit word
+            // for the desired pixel position on the screen
+            uint16_t value = ((machine->shift1 << 8) | machine->shift0);
+            i8080->regs.a = ((value >> (8 - machine->shift_offset) & 0xFF));
+            break;
+        case 0x00:
+        case 0x01:
+        case 0x02:
+        default:
+            break;
+    }
+}
+
+void space_invaders_out(intel8080 *i8080, uint8_t port) {
+    /*
+     * Write
+     * 02 shift amount (3 bits)
+     * 03 sound bits
+     * 04 shift data
+     * 05 sound bits
+     * 06 watch-dog
+     */
+    SpaceInvaders *machine = (SpaceInvaders*)i8080->userdata;
+    switch(port) {
+        case 0x02:
+            machine->shift_offset = i8080->regs.a & 0x7;
+            break;
+        case 0x03:
+        case 0x04:
+            machine->shift0 = machine->shift1;
+            machine->shift1 = i8080->regs.a;
+            break;
+        case 0x05:
+        case 0x06:
+        default:
+            break;
+    }
+}
+
+static bool init_space_invaders(SpaceInvaders *machine) {
+    if(!init_8080(&machine->i8080, "./space_invaders/rom/invaders.rom", 0x0000, machine)) {
         return false;
     }
 
-    InitWindow(SCREEN_WIDTH * cpu->io.scale_factor, SCREEN_HEIGHT * cpu->io.scale_factor, "Space Invaders");
-    SetTargetFPS(60);
+    memset(machine->io.display, 0, sizeof(machine->io.display));
+    machine->io.scale_factor = 3;
+    
+    set_in(space_invaders_in);
+    set_out(space_invaders_out);
 
-    while(!WindowShouldClose() && !cpu->quit) {
-        BeginDrawing();
-        ClearBackground(YELLOW);
-        EndDrawing();
+    return true;
+}
+
+int main() {
+    SpaceInvaders machine;
+    intel8080 *i8080 = &machine.i8080;
+
+    if(!init_space_invaders(&machine)) {
+        return EXIT_FAILURE;
     }
 
-    destroy_8080(cpu);
-    return 1;
+    InitWindow(SCREEN_HEIGHT * machine.io.scale_factor, SCREEN_WIDTH * machine.io.scale_factor, "Space Invaders");
+    SetTargetFPS(FPS);
+
+    double last_frame = GetTime();
+
+    /*
+     * 2MHz == 2,000,000 hz / 60 FPS = 33,3333 instructions per frame
+     * Frame time = 1 second / 60 FPS = 16.6667 ms
+     *
+     *
+     * CRT displays images line by line, from top to bottom, then returns to
+     * the top through a vertical blank interrupt. 
+     *
+     *  - RST1 - The VBL roughly reached the middle of the screen
+     *  - RST2 - The VBL reached the bottom of the screen
+     *
+     * Between interrupts, the CPU will run roughly 16,667 cycles, or
+     * roughly 33,333 cycles for the entire screen per frame.
+     *
+     */
+    while(!WindowShouldClose() && !i8080->quit) {
+        last_frame = GetTime();
+        handle_input(&machine);
+
+        while(i8080->cycles < INSTR_PER_FRAME / 2) {
+            emulate_8080(i8080);
+        }
+        generate_interrupt(i8080, 0xCF); // RST 1
+
+        while(i8080->cycles < INSTR_PER_FRAME) {
+            emulate_8080(i8080);
+        }
+        generate_interrupt(i8080, 0xD7); // RST 2
+
+        ClearBackground(BLACK);
+        BeginDrawing();
+        draw_display(&machine);
+        EndDrawing();
+
+        double elapsed = GetTime() - last_frame;
+        if(elapsed > FRAME_TIME) {
+            WaitTime(elapsed);
+        }
+        i8080->cycles = 0;
+    }
+
+    destroy_8080(i8080);
+
+    return EXIT_SUCCESS;
 }
---
+++ space_invaders/space_invaders.h
@@ -0,0 +1,76 @@
+#include "raylib.h"
+#include "intel8080.h"
+
+#define FPS (60)
+#define FRAME_TIME (1/FPS)
+#define INSTR_PER_FRAME (CPU_MHZ / FPS)
+
+typedef struct {
+    struct {
+        Color       display[SCREEN_WIDTH*SCREEN_HEIGHT];
+        uint8_t     scale_factor;
+    } io;
+
+    uint8_t shift0;         // LSB of the machine's shift hardware
+    uint8_t shift1;         // MSB
+    uint8_t shift_offset;   // Offset for machines shift hardware
+
+    intel8080 i8080;
+} SpaceInvaders;
+
+/*
+ *
+ * Read
+ * ============
+ * 00 INPUTS (Mapped in hardware but never used by the code)
+ * 01 INPUTS
+ * 02 INPUTS
+ * 03 bit shift register read
+ * 
+ * Write
+ * ============
+ * 02 shift amount (3 bits)
+ * 03 sound bits
+ * 04 shift data
+ * 05 sound bits
+ * 06 watch-dog 
+ */
+typedef enum {
+    // IN PORT 1
+    CREDIT      = 1,
+    START_2P 	= (1 << 1),
+    START_1P 	= (1 << 2),
+    UNUSED   	= (1 << 3), // Always 1
+    SHOOT_1P  	= (1 << 4),
+    LEFT_1P  	= (1 << 5),
+    RIGHT_1P 	= (1 << 6),
+
+    // IN PORT 2
+    DIP3     	= (1 << 0),
+    DIP5     	= (1 << 1),
+    TILT     	= (1 << 2),
+    DIP6     	= (1 << 3),
+    SHOOT_2P  	= (1 << 4),
+    LEFT_2P  	= (1 << 5),
+    RIGHT_2P 	= (1 << 6),
+    DIP7     	= (1 << 7),
+
+    // OUT PORT 3
+    UFO_SFX     = (1 << 0),
+    SHOOT_SFX   = (1 << 1),
+    PLAYER_DIE  = (1 << 2),
+    INVADER_DIE = (1 << 3),
+    EXT_PLAY    = (1 << 4),
+    AMP_ENABLE  = (1 << 5),
+
+    // OUT PORT 5
+    FLEET_1     = (1 << 0),
+    FLEET_2     = (1 << 1),
+    FLEET_3     = (1 << 2),
+    FLEET_4     = (1 << 3),
+    UFO_HIT     = (1 << 4),
+
+} PORT_BITS;
+
+void space_invaders_in(intel8080 *i8080, uint8_t port);
+void space_invaders_out(intel8080 *i8080, uint8_t port);
